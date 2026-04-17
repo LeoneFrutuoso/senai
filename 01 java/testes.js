be09ded4515abe5fd55ab5dce58ef4001dd6a731
LeoneFrutuoso <leonefrutuosoferreira@gmail.com>
2026-04-16 22:20:08 -0300

    exercicio java

---
+++ 01 java/testes.js
@@ -0,0 +1,30 @@
+// //alert("olà mundo")
+// //alert("segunda mensagem")
+
+// let nome
+
+
+// nome ="Leone"
+
+// alert("nome " + nome)
+
+// let idade =30
+// alert("idade" + idade)
+
+// alert("nome: " +  "\nIdade: "+ idade )
+
+// nome = prompt("digite seu nome")
+// alert ("nome digitado" + nome)
+
+
+let nota
+ prompt("digite a primeira nota")
+ nota1 = Number(1)
+
+ alert(nota1)
+
+
+
+
+
+
