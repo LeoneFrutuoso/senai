6cbb8cb867e9b5e9556bf9fdc6536ca3976b973e
FRANCISCA LEONE FRUTUOSO FERREIRA <francisca_ferreir151@estudante.sesisenai.org.br>
2026-04-28 22:13:00 -0300

    css

--- css/romeroBrique.js
+++ css/romeroBrique.js
@@ -1,15 +1,15 @@
-function calcularprecoBrique(){
+function calcularPrecoBrique(){
 // infor
 let precoCompra,precoVenda
 // leitura
-precoCompra= number(prompt("preco de compra:"))
+precoCompra= Number(prompt("preco de compra:"))
 
 // processamento
 precoVenda = precoCompra *3
 //   saidas
 console.log("precovenda: rs"+precoVenda.toFixed(2))
-alert (precoparavenda) 
-document.getElementById("resultado").innerHTML = "precovenda" .toFixed((2))
+alert (precoVenda) 
+document.getElementById("resultado").innerHTML = precovendag.toFixed((2))
 
 
 
