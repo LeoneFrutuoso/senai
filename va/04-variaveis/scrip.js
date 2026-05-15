66c4e7246ad871d9f21d48de2fee00d836c7f034
Operadores <leonefrutuosoferreira@gmail.com>
2026-05-14 22:09:27 -0300

    variaveis

---
+++ va/04-variaveis/scrip.js
@@ -0,0 +1,63 @@
+letcon = 0
+function incrementarContagem(){
+let cont = 0
+cont = cont + 1
+document.getElementById('p-contagem').innerHTML = cont
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let global ='eu sou uma variavel global :/'
+function funcao1(){
+    let local ='sou uma variavel :D'
+    console.log(local)
+    console.log(global)
+}
+ function funcao2(){
+    console.log(global);
+    // console.log(local)
+ }
+
+//  funcao1()
+//  funcao2()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
