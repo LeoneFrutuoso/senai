46b4b89b5d4e29350050ff23675d6ee1b8cf3194
Operadores <leonefrutuosoferreira@gmail.com>
2026-05-18 21:51:19 -0300

    variaveis

---
+++ va/04-variaveis/script.js
@@ -0,0 +1,123 @@
+//  total de vendas: r$
+let total= 0
+function registrarVenda(){
+let valor = Number(document.getElementById('inputVenda').value)
+// registrar venda no total
+total = total + valor 
+// atualizar total na pagina 
+document.getElementById('totalVendido').innerHTML =
+'total de vendas: R$' + total.toFixed(2)
+//  lista de vendas 
+document.getElementById('listaVendas').innerHTML += 'R$' + valor.toFixed(2) +'<br>'
+document.getElementById('inputVenda').value = ''
+document.getElementById('inputVenda').focus()
+}
+
+
+
+
+
+
+
+
+
+function registrarVenda(){
+let valor = Number(document.getElementById('inputVenda').value)
+    // registrar venda no total
+    // atualizar total na página
+
+    console.log(valor)
+    
+}
+
+
+
+
+
+
+letcon = 0
+function incrementarContagem(){
+cont = cont + 1
+    // cont = cont + 1
+    // cont += 1
+    cont++
+    document.getElementById('p-contagem').innerHTML = cont
+
+}
+
+
+function decrementarContagem(){
+   cont--
+    document.getElementById('p-contagem').innerHTML = cont
+}
+function somar10(){
+    cont = cont + 10
+    // cont += 10
+    document.getElementById('p-contagem').innerHTML = cont
+}
+function somar2(){
+    cont += 2
+    document.getElementById('p-contagem').innerHTML = cont
+}
+function zerarContagem(){
+    cont = 0
+    document.getElementById('p-contagem').innerHTML = cont
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let global ='eu sou uma variavel global :/'
+function funcao1(){
+    let local ='sou uma variavel :D'
+    console.log(local)
+    console.log(global)
+}
+ function funcao2(){
+    console.log(global);
+    // console.log(local)
+ }
+
+//  funcao1()
+//  funcao2()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
