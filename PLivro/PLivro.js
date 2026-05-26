2b70af1b16bcd71f6a18ac48983ff51aed90ef77
LeoneFrutuoso <LeonefrutuosoFerreira@gmail.com>
2026-05-26 00:49:09 -0300

    PLivro

---
+++ PLivro/PLivro.js
@@ -0,0 +1,66 @@
+let totalContador = 0
+function add1(){
+alert("voce clicou no botao1")
+let add1
+totalContador = totalContador + add1
+ document.getElementById("contador").innerHTML = totalContador
+}
+
+function add2(){
+alert("voce clicou no botao2")
+let add2
+totalContador = totalContador + add2
+ document.getElementById("contador").innerHTML = totalContador
+}
+ function  add4(){
+alert("voce clicou no botao4")
+let add4
+totalContador = totalContador + add4
+ document.getElementById("contador").innerHTML = totalContador
+ }
+
+function add10(){
+alert("voce clicou no botao10")
+let add10
+totalContador = totalContador + add10
+ document.getElementById("contador").innerHTML = totalContador
+
+
+}
+function  add50(){
+alert("voce clicou no botao50")
+let add50
+totalContador = totalContador + add50
+ document.getElementById("contador").innerHTML = totalContador
+
+
+}
+
+function  add100(){
+alert("voce clicou no botao 100")
+let add100
+totalContador = totalContador + add100
+ document.getElementById("contador").innerHTML = totalContador
+
+}
+
+ function diminui1(){
+let diminui1
+totalContador = totalContador - diminui1
+ document.getElementById("contador").innerHTML = totalContador
+}
+
+function zerar (){
+// total contador = 0
+let zerar
+totalContador = zerar * totalContador 
+ document.getElementById("contador").innerHTML = totalContador
+
+
+}
+
+
+
+
+
+ 
