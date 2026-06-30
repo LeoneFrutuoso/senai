5e8c769bad8ad792fa77a62da16c84caab1b48cf
FRANCISCA LEONE FRUTUOSO FERREIRA <francisca_ferreir151@estudante.sesisenai.org.br>
2026-06-29 22:07:10 -0300

    array

---
+++ arrays/script.js
@@ -0,0 +1,68 @@
+
+     let idades = [18, 20, 12, 35, 14, 15]
+     let precos =[]
+
+     const produtos = [
+  "Camiseta Algodão", "Calça Jeans", "Tênis Esportivo", "Relógio de Pulso", "Mochila Escolar", "Fone de Ouvido", "Garrafa Térmica", "Jaqueta Corta-Vento", "Boné Aba Reta", "Óculos de Sol", "Carteira de Couro", "Cinto Social", "Meia Cano Alto", "Moletom com Capuz", "Bermuda Cargo", "Camisa Polo", "Sapato Social", "Chinelo de Dedo", "Perfume Importado", "Creme Hidratante", "Shampoo Anticaspa", "Protetor Solar", "Escova de Dentes", "Toalha de Banho", "Travesseiro Nasa", "Luminária de Mesa", "Teclado Mecânico", "Mouse Sem Fio", "Mousepad Gamer", "Carregador Portátil", "Caixa de Som Bluetooth", "Cabo HDMI", "Suporte para Notebook", "Pendrive 64GB", "Smartwatch", "Agenda 2026", "Caneta Esferográfica", "Caderno Universitário", "Pasta Executiva", "Organizador de Cabos", "Caneca de Cerâmica", "Garrafa de Vidro", "Avental de Cozinha", "Jogo de Facas", "Fradeira Elétrica", "Jogo de Chaves", "Trena Métrica", "Lanterna LED", "Cadeado de Latão", "Mala de Viagem"
+];
+function mostrarProdutos() {
+for (let i = 0; i < produtos.length; i++) {
+    console.log(produtos[i]);
+    document.getElementById('lista-precos').innerHTML += '<p>' + produtos[i] + '</p>';  
+}
+
+
+
+
+}
+
+     //prompt para riar array de nome de produtos array js, um unico
+     //  e simples array de string com 50 posiçoes e um nome de produto em cada posiçao
+
+
+
+
+
+
+// function array00() {
+
+    // let i=14
+    // console.log("idades" + idades[i])
+
+ //     console.log("idades" + idades[0])
+// console.log("idades" + idades[1])
+// console.log("idades" + idades[2])
+// console.log("idades" + idades[3])
+// console.log("idades" + idades[4])
+// console.log("idades" + idades[5])
+   
+
+// for (let i=0; i <6; i++)
+//     {console.log("idades" + idades[i])}
+//     }
+
+   
+// for (let i=0; i < idades.length; i++)
+//     {console.log("idades" + idades[i])}
+    
+// }
+
+//}
+// function addPrecos() {
+// let n = Math.ceil(Math.random() * 100)
+//     precos.push(n)
+// //console.log(precos)
+
+
+
+
+// document.getElementById('precos').innerHTML = ' '
+// for (let i=0; i < precos.length; i++){
+//     console.log( precos[i])
+// document.getElementById('precos').innerHTML +=  + precos[i] + '-'
+// }
+
+// }
+
+
+
